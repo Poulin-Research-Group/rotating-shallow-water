fixed major error in fortran calculation of q; fortran code for calculating energy and enstrophy was added (and it works, too!)

sadourny.py
@@ -140,9 +140,8 @@ def main(method, sc):
     print t_final - t_start
 
     # PLOTTING ==========================================================================
-    PLOTTO_649(UVH, x, y, Ny, N, './anims/sadourny.mp4')
+    # PLOTTO_649(UVH, x, y, Ny, N, './anims/sadourny.mp4')
 
-    """
     print "Error in energy is ", np.amax(energy-energy[0])/energy[0]
     print "Error in enstrophy is ", np.amax(enstr-enstr[0])/enstr[0]
 
@@ -153,7 +152,6 @@ def main(method, sc):
     ax1.set_title('Energy')
     ax2.plot((enstr-enstr[0])/enstr[0], '-or',  linewidth=2, label='Enstrophy')
     ax2.set_title('Enstrophy')
-    plt.show()
-    """
+    plt.savefig('./anims/ener-enst-FORTRAN.png')
 
 main(flux_sw_ener_F, 1)

sadourny_setup.py
@@ -4,7 +4,8 @@ import matplotlib.animation as animation
 import sys
 import time
 from mpi4py import MPI
-from flux_ener import calc_h, calc_u, calc_v, calc_b, calc_q, calc_flux_1, calc_flux_2, calc_flux_3
+from flux_ener import calc_h, calc_u, calc_v, calc_b, calc_q, calc_flux_1, calc_flux_2, calc_flux_3, \
+                      calc_energy, calc_enstrophy
 comm = MPI.COMM_WORLD
 
 
@@ -159,8 +160,10 @@ def flux_sw_ener_F(uvh, params):
                       calc_flux_3(U, V, dx, dy)])
 
     # compute energy and enstrophy
-    energy = 0.5*np.mean(gp*h**2 + h*(axm(uvh[Iu_i:Iu_f, :]**2) + aym(uvh[Iv_i:Iv_f, :]**2)))
-    enstrophy = 0.5*np.mean(ayp(axp(h))*q**2)
+    # energy = 0.5*np.mean(gp*h**2 + h*(axm(uvh[Iu_i:Iu_f, :]**2) + aym(uvh[Iv_i:Iv_f, :]**2)))
+    energy = calc_energy(uvh, h, gp, Iv_i, Iv_f)
+    # enstrophy = 0.5*np.mean(ayp(axp(h))*q**2)
+    enstrophy = calc_enstrophy(h, q)
 
     return flux, energy, enstrophy
 